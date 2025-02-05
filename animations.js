fa8d0b5e26db1c9525b037ef01ff73bd35fa6120
Schuy <schuylertk@gmail.com>
2025-02-05 16:21:48 -0700

    Made it one button, more intuitive

--- animations.js
+++ animations.js
@@ -1,35 +1,62 @@
-const messages = [
+const all_messages = [
 	"Do you know!\nWhat's on the Valentine's Day menu?\nMe n U :)",
+	"Hey!\nGuess what's on the Valentine's menu?\nStill hoping it's Me + u :)",
 	"\uD83D\uDC9E You had me at \"Hello\" :)",
 	"The things I'd do for a little chocolate \uD83D\uDC95",
 	"Hey you!\nNot sure if cupid messed up but\nhere I am :)",
+	"I was never into calculus but\nU n Me?\nMight add up :)",
+	"U + Me = a decent team\nWanna try it out? :)",
+	"Be my sorta valentine?\nWe can work out the details later",
+	"We get together like wifi and meme\nalways connected ;)",
+	"Hey, fun fact!\nThis Valentine's Day recipe call\nfor the two of us \uD83D\uDC97",
+	"Heads up!\nThe best menu special I found this year\nIs U :)",
+	"FYI\nCupids looking for an update - \nMight as well put our names in!",
+	"Pop Quiz!\nWhat sounds better: chocolates alone?\nOr chocolates with me? :)",
 ]
-
+let messages = structuredClone(all_messages);
 let opened = false;
-let changed = false;
+let changed = 0;
+
+// Changes the message on the card.
+// Returns: changed, as changed is not reference and functions as a bool.
+function changeMessage(messages, changed) {
+	let text = document.getElementById("message");
+	let new_text = null;
+
+	// Reset array if exhausted options
+	if (messages.length === 0) {
+		changed = 0;
+		messages = structuredClone(all_messages);
+	}
+
+	let index = Math.floor(Math.random() * messages.length);
+
+	if (changed === 0 || changed === 1) {
+		index = 0;
+		new_text = messages[0];
+	} else {
+		new_text = messages[index];
+	}
+	changed++;
+	let final_text = document.createTextNode(new_text);
+	while (text.firstChild) {
+		text.removeChild(text.firstChild);
+	}
+	text.appendChild(final_text);
+
+	messages.splice(index, 1);
+
+	return changed;
+}
+
 document.getElementById("openButton").addEventListener("click", function() {
+	const lid = document.querySelector(".lid");
+	const card = document.querySelector(".card");
+
+
 	if (!opened) {
-		const lid = document.querySelector(".lid");
-		const card = document.querySelector(".card");
-		let text = document.getElementById("message");
-		let new_text = null;
-
-		if (!changed) {
-			changed = true;
-			new_text = messages[0];
-		} else {
-			new_text = messages[Math.floor(Math.random() * messages.length)];
-		}
-
-		console.log(new_text);
-		console.log(text);
-		let final_text = document.createTextNode(new_text);
-		while (text.firstChild) {
-			text.removeChild(text.firstChild);
-		}
-		text.appendChild(final_text);
-
 		// Tranform lid
+		changed = changeMessage(messages, changed);
 		lid.style.transform = "rotatex(-180deg)";
 
 		setTimeout(() => {
@@ -37,20 +64,31 @@ document.getElementById("openButton").addEventListener("click", function() {
 			card.style.transform = "translatey(-9.5vmin)";
 		}, 400);
 		opened = true;
-	}
-});
 
-document.getElementById("resetButton").addEventListener("click", function() {
-	if (opened) {
-		const lid = document.querySelector(".lid");
-		const card = document.querySelector(".card");
-
+	} else {
 		card.style.transform = "translatey(0)";
 
+		// Close lid
 		setTimeout(() => {
 			card.style.zIndex = "1";
 			lid.style.transform = "rotatex(0deg)";
-		}, 400);
-		opened = false;
+		}, 500);
+
+		// Change message
+		setTimeout(() => {
+			changed = changeMessage(messages, changed);
+		}, 900);
+
+		// Open lid
+		setTimeout(() => {
+			lid.style.transform = "rotatex(-180deg)";
+		}, 1400);
+
+		// lift the card
+		setTimeout(() => {
+			card.style.zIndex = "3";
+			card.style.transform = "translatey(-9.5vmin)";
+		}, 1800);
 	}
 });
+
